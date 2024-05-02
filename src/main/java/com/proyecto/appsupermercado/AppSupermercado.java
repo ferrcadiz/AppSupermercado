836fffe8324df98c5c83d5d598a7194af46d9baf
benjaaacf <benjamin.caballeria.f@mail.pucv.cl>
2024-05-02 03:55:00 -0400

    carga3

--- src/main/java/com/proyecto/appsupermercado/AppSupermercado.java
+++ src/main/java/com/proyecto/appsupermercado/AppSupermercado.java
@@ -20,7 +20,9 @@ public class AppSupermercado {
         Inventario inventario = new Inventario();
 
         
-        
+        String txtProveedores = "C:\\Users\\bpcab\\Desktop\\AppSupermercado(N)\\proveedores.txt";
+        String txtProductos = "C:\\Users\\bpcab\\Desktop\\AppSupermercado(N)\\productos.txt";
+        String txtReporte = "C:\\Users\\bpcab\\Desktop\\AppSupermercado(N)\\reporte.txt";
 
         inventario.cargarProveedores(txtProveedores);
         System.out.println("Proovedores cargados! :D");
